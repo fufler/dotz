2aba3bac511a28d61eca2773ecad605f8cb3036a
Alexey Ermakov <fufler@gmail.com>
2013-09-03 16:26:59 +0500

    Midnight Commander config file and skin

--- install.sh
+++ install.sh
@@ -1,11 +1,23 @@
 #!/bin/zsh
 
+sln() {
+  ln -iTvs "$1" "$2"
+}
+
 DIR=`pwd`
 
 setopt EXTENDED_GLOB
 for rcfile in prezto/runcoms/^README.md(.N); do
-  ln -iTvs "$DIR/$rcfile" "$HOME/.${rcfile:t}"
+  sln "$DIR/$rcfile" "$HOME/.${rcfile:t}"
 done
 
-ln -iTvs "$DIR/zsh" "$HOME/.zsh"
-ln -iTvs "$DIR/prezto" "$HOME/.zprezto"
+sln "$DIR/zsh" "$HOME/.zsh"
+sln "$DIR/prezto" "$HOME/.zprezto"
+
+mkdir -p "$HOME/.config/mc"
+sln "$DIR/mc/ini" "$HOME/.config/mc/ini"
+
+mkdir -p "$HOME/.local/share/mc/skins"
+for sfile in mc/skins/*.ini(.N); do
+  sln "$DIR/$sfile" "$HOME/.local/share/mc/skins/${sfile:t}";
+done
