363a0e842d73437fe54e2b6894ae959f982a94c2
Alexey Ermakov <fufler@gmail.com>
2015-01-20 19:35:47 +0300

    More dnf aliases

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -3,4 +3,16 @@
 if [[ ! "$UID" -eq "0"  ]]; then
     alias dnf='sudo dnf';
     alias yum='sudo yum';
+
+    alias dnfi='sudo dnf install';
+    alias dnfiy='sudo dnf install -y';
+
+    alias dnfs='sudo dnf search';
+    alias dnfp='sudo dnf provides';
+
+    alias dnfr='sudo dnf remove';
+    alias dnfry='sudo dnf remove -y';
+
+    alias dnfu='sudo dnf update';
+    alias dnfuy='sudo dnf update -y';
 fi
