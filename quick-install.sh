f76f53ea40eed1e7c3392bfe3aec5caf3e0494dd
Alexey Ermakov <fufler@gmail.com>
2016-09-27 08:38:11 +0300

    Install to ~/.dotz folder

--- quick-install.sh
+++ quick-install.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-git clone --recurse-submodules https://github.com/fufler/dotz.git
-cd dotz
+git clone --recurse-submodules https://github.com/fufler/dotz.git ~/.dotz
+cd ~/.dotz
 zsh ./install.sh
