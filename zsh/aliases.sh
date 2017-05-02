e353f32cff3b37752d57f00f18279718c364f100
Alexey Ermakov <fufler@gmail.com>
2017-05-02 21:24:42 +0300

    Update ~/.zshrc

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,18 +1,6 @@
 #!/bin/zsh
 
 if [[ ! "$UID" -eq "0"  ]]; then
-    alias dnf='sudo dnf';
-    alias yum='sudo yum';
-
-    alias dnfi='sudo dnf install';
-    alias dnfiy='sudo dnf install -y';
-
-    alias dnfs='sudo dnf search';
-    alias dnfp='sudo dnf provides';
-
-    alias dnfr='sudo dnf remove';
-    alias dnfry='sudo dnf remove -y';
-
-    alias dnfu='sudo dnf update';
-    alias dnfuy='sudo dnf update -y';
+    alias emerge='sudo emerge'
+    alias xmerge='FEATURES="candy distcc distcc-pump ccache" MAKEOPTS="-j6 -l0" sudo -E emerge';
 fi
