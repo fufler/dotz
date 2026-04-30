3a5fc425e380874c88cb769568f7dfcab37e1ad2
Alexey Ermakov <fufler@gmail.com>
2026-04-30 16:08:44 +0300

    Configure lsp fom neovim

--- nvim/lua/plugins/astrolsp.lua
+++ nvim/lua/plugins/astrolsp.lua
@@ -3,6 +3,13 @@ return {
   "AstroNvim/astrolsp",
   ---@type AstroLSPOpts
   opts = {
+    features = {
+      inlay_hints = true,
+      signature_help = true,
+      inline_completion = true,
+      linked_editing_range = false,
+      semantic_tokens = true
+    },
     formatting = {
       format_on_save = {
         enabled = false
